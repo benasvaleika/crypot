86fe23408c0e6e6529e34c22acd686b2927ccaaf
Bentini <bentini113@gmail.com>
2021-10-08 21:08:12 +0300

    !price command added

--- src/commands/getPrice.ts
+++ src/commands/getPrice.ts
@@ -3,11 +3,27 @@ const bitvavo = require("bitvavo")();
 module.exports = {
   name: "getPrice",
   description: "Gets price of a crpyto asset",
-  async execute(message: any, args?: any) {
+  async execute(message: any, args: any) {
+    try {
+      let response = await bitvavo.balance({});
+      for (let entry of response) {
+        console.log(entry);
+      }
+    } catch (error) {
+      console.log(error);
+    }
     try {
       let response = await bitvavo.tickerPrice({});
       for (let entry of response) {
-        console.log(entry);
+        if (entry.market === "NANO-EUR") {
+          message.channel.send(`NANO kaina: ${entry.price}€`);
+        } else if (entry.market === "BTC-EUR") {
+          message.channel.send(`BTC kaina: ${entry.price}€`);
+        } else if (entry.market === "ETH-EUR") {
+          message.channel.send(`ETH kaina: ${entry.price}€`);
+        } else if (entry.market === "SOL-EUR") {
+          message.channel.send(`SOLANA kaina: ${entry.price}€`);
+        }
       }
     } catch (error) {
       console.log(error);
